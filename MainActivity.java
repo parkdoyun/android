0f9bef2f46f469d1fb1833aa73a901c54d890540
parkdoyun <ehdqkdfjq97@gmail.com>
2020-02-07 02:11:19 +0900

    명화 투표 순서대로 자동 보기

--- MainActivity.java
+++ MainActivity.java
@@ -11,26 +11,26 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 // 먼저 투표하는 코드
+// 투표 수 저장한 배열과 파일명 저장한 배열 ResultActivity에 넘겨줌
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("명화 선호도 투표");
+        setTitle("명화 투표");
 
 
         final int voteCount[] = new int[9];
-        for(int i = 0; i < 9; i++)
-        {
+        for (int i = 0; i < 9; i++) {
             voteCount[i] = 0;
         }
 
         ImageView image[] = new ImageView[9];
         Integer imageId[] = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
         //final Integer imageFileId[] = {R.drawable.renoir1, R.drawable.renoir2, R.drawable.renoir3, R.drawable.renoir4,R.drawable.renoir5, R.drawable.renoir6,
-                //R.drawable.renoir7, R.drawable.renoir8, R.drawable.renoir9}; -> Integer 배열로 넘기면 값 안 넘겨짐!
-        final ArrayList<Integer>  imageFileId = new ArrayList<Integer>(); // ArryList로 넘거야함!
+        //R.drawable.renoir7, R.drawable.renoir8, R.drawable.renoir9}; -> Integer 배열로 넘기면 값 안 넘겨짐!
+        final ArrayList<Integer> imageFileId = new ArrayList<Integer>(); // ArryList로 넘거야함!
         imageFileId.add(R.drawable.renoir1);
         imageFileId.add(R.drawable.renoir2); // 파일 이름들 추가.
         imageFileId.add(R.drawable.renoir3);
@@ -42,11 +42,10 @@ public class MainActivity extends AppCompatActivity {
         imageFileId.add(R.drawable.renoir9);
 
         final String imgName[] = {"도시의 무도회", "푸르네즈에서의 점심 식사", "물뿌리개를 든 소녀",
-        "뱃놀이 일행의 오찬", "아르장퇴유의 정원에서", "작은 배", "잔 사마리의 초상",
-        "해변에서", "호숫가 근처 물가에서"};
+                "뱃놀이 일행의 오찬", "아르장퇴유의 정원에서", "작은 배", "잔 사마리의 초상",
+                "해변에서", "호숫가 근처 물가에서"};
 
-        for(int i = 0; i < imageId.length; i++)
-        {
+        for (int i = 0; i < imageId.length; i++) {
             final int index; // **주의!! 꼭 필요함!!**
             index = i;
             image[index] = (ImageView) findViewById(imageId[index]); // 각각 매칭
@@ -65,19 +64,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
                 intent.putExtra("VoteCount", voteCount); // 정수형 배열 voteCount를 VoteCount라는 이름으로 넘긴다 -> getIntArrayExtra()
-                intent.putExtra("ImageName", imgName); // -> getStringArrayExtra()
+                //intent.putExtra("ImageName", imgName); // -> getStringArrayExtra()
                 intent.putIntegerArrayListExtra("FileName", imageFileId); // ***Integer는 이렇게 넘겨야 함!!!
                 startActivity(intent);
             }
         });
     }
-
-
-
-
-
-
-
 
 
 }
--- ResultActivity.java
+++ ResultActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.util.List;
-// 투표한 후 결과 보여준다.
-// 결과랑 최다 득표한 그림 같이 ImageView와 TextView에 띄운다.
+
+//값 받아서 1등부터 9등까지 순서대로 이미지뷰에 넣고
+//뷰 플리퍼로 자동보기 기능 넣어야한다.
 public class ResultActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,47 +19,60 @@ public class ResultActivity extends Activity {
         setContentView(R.layout.result);
         setTitle("투표 결과");
 
-        Intent intent = getIntent(); // 인텐트 받음
+        Intent intent = getIntent();
         int[] voteResult = intent.getIntArrayExtra("VoteCount"); // 넘겨받은 배열 변수 저장
-        String[] imageName = intent.getStringArrayExtra("ImageName");
         List<Integer> fileName = intent.getIntegerArrayListExtra("FileName"); // int형이 아니라 Integer였다는 거 조심!!
         // ***Integer형은 받을 때 getIntegerArrayListExtra() 즉, 배열이 아닌 List(ArrayList)로 받아야 한다!!!(아님 에러)***
 
-        int maxVoteIdx = 0; // 최다 득표 그림의 인덱스
-        int maxVote = 0; // 최다 득표 수
-
-        TextView tv[] = new TextView[imageName.length];
-        RatingBar rbar[] = new RatingBar[imageName.length];
-
-        Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
-        Integer rbarID[] = {R.id.rBar1, R.id.rBar2, R.id.rBar3, R.id.rBar4, R.id.rBar5, R.id.rBar6, R.id.rBar7, R.id.rBar8, R.id.rBar9};
-
-        for (int i = 0; i < voteResult.length; i++) {
-            tv[i] = (TextView) findViewById(tvID[i]); // mapping
-            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        int[] order = new int[9]; // 1등부터 투표수대로 인덱스 담을 배열 생성
+        int[] cntCheck = new int[9]; // 해당 인덱스가 이미 order[]배열에 있는 인덱스인지 아닌지 확인하기 위해 생성 -1 : check x, 0 : check
+        for (int i = 0; i < 9; i++) {
+            cntCheck[i] = -1; // 초기화
         }
+        int max = -1; // 투표 수 최대값 저장할 변수.
+        int max_idx = -1; // 최댓값의 인덱스 저장할 변수.
 
-        for (int i = 0; i < voteResult.length; i++) {
-            tv[i].setText(imageName[i]); // 그림 파일 이름 적용
-            rbar[i].setRating((float) voteResult[i]); // 투표수 적용
-        }
-        for (int i = 0; i < voteResult.length; i++) { // 최다 득표한 그림 찾기.
-            if (maxVote < voteResult[i]) {
-                maxVoteIdx = i;
-                maxVote = voteResult[i];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (cntCheck[j] == 0) continue; // 이미 체크된 항목 제외
+                if (voteResult[j] > max) {
+                    max = voteResult[j]; // 최댓값 갱신
+                    max_idx = j; // 인덱스 갱신
+                }
             }
+            order[i] = max_idx;
+            cntCheck[max_idx] = 0; // 체크
+            max = -1; // 최댓값 초기화
         }
-        TextView tvResult = (TextView) findViewById(R.id.tvResult);
-        tvResult.setText(imageName[maxVoteIdx]);
-        ImageView imgResult = (ImageView) findViewById(R.id.imgResult);
-        imgResult.setImageResource(fileName.get(maxVoteIdx)); // **이렇게 받아야 된다!
+        int[] imgName = {R.id.img1, R.id.img2, R.id.img3, R.id.img4, R.id.img5, R.id.img6, R.id.img7, R.id.img8, R.id.img9}; // 이미지 아이디 배열
+
+        ImageView[] img = new ImageView[9];
+
+        for (int i = 0; i < 9; i++) {
+            img[i] = (ImageView) findViewById(imgName[i]); // mapping.
+        }
+
+        for (int i = 0; i < 9; i++) // 순서대로 이미지 파일에 넣어줌.
+        {
+            img[i].setImageResource(fileName.get(order[i])); // order 배열의 인덱스 순서대로 파일 집어넣기
+        }
 
 
-        Button btnReturn = (Button) findViewById(R.id.btnReturn);
-        btnReturn.setOnClickListener(new View.OnClickListener() {
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        final ViewFlipper vFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                vFlipper.setFlipInterval(2000); // ms이므로 (1000ms = 1s)
+                vFlipper.startFlipping();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vFlipper.stopFlipping();
             }
         });
     }
